76f97fd0ac76e410e11b84b1a6afd0963d1a9545
Peixotim <pedropeixotovz@gmail.com>
2026-03-28 07:18:30 -0300

    feat: Adiciona script para geração de migrações

---
+++ src/scripts/createMigration.ts
@@ -0,0 +1,32 @@
+import fs from "fs";
+import path from "path";
+
+const migrationsDir = path.join(__dirname, "../migrations");
+
+function generateMigration(name: string) {
+  const timestamp = new Date().toISOString().replace(/[-T:.Z]/g, "");
+  const filename = `${timestamp}-${name}.ts`;
+  const filepath = path.join(migrationsDir, filename);
+
+  const template = `import { QueryInterface, DataTypes } from 'sequelize';
+
+export async function up(queryInterface: QueryInterface) {
+  // TODO: implement migration
+}
+
+export async function down(queryInterface: QueryInterface) {
+  // TODO: revert migration
+}
+`;
+
+  fs.writeFileSync(filepath, template);
+  console.log(`Migration created: ${filename}`);
+}
+
+const migrationName = process.argv[2];
+if (!migrationName) {
+  console.error("Please provide a migration name.");
+  process.exit(1);
+}
+
+generateMigration(migrationName);
