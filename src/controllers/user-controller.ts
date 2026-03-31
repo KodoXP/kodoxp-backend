17b6355e7956e39fc8b608784e619fcbb3f456ed
Peixotim <pedropeixotovz@gmail.com>
2026-03-31 11:44:10 -0300

    feat: Implements UserController with findById method to search for user by ID.

--- src/controllers/user-controller.ts
+++ src/controllers/user-controller.ts
@@ -1,1 +1,17 @@
-export class UserController {}
+import { UserService } from "@/services/user-service";
+import { Request, Response, NextFunction } from "express";
+
+export class UserController {
+  constructor(private readonly userService: UserService) {}
+
+  public async findById(req: Request, res: Response, next: NextFunction) {
+    try {
+      const { id } = req.params;
+      const user = await this.userService.findById(id);
+
+      res.status(200).json(user);
+    } catch (error) {
+      next(error);
+    }
+  }
+}
