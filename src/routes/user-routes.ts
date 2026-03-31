ea9a34da883795b03884d3542fca6c651bd082d7
Peixotim <pedropeixotovz@gmail.com>
2026-03-31 16:25:10 -0300

    feat: add create user route and update validateSchema usage for findById

--- src/routes/user-routes.ts
+++ src/routes/user-routes.ts
@@ -1,7 +1,7 @@
 import { UserController } from "@/controllers/user-controller";
 import { validateSchema } from "@/middlewares/validate-schema";
 import { UserRepository } from "@/repositories/user-repository";
-import { findById } from "@/schemas/user-schemas";
+import { createUserSchema, findById } from "@/schemas/user-schemas";
 import { UserService } from "@/services/user-service";
 import { Router } from "express";
 
@@ -10,6 +10,11 @@ const userRepostiory = new UserRepository();
 const userService = new UserService(userRepostiory);
 const controller = new UserController(userService);
 
-router.get("/users/:id", validateSchema(findById), controller.findById.bind(controller));
+router.get("/users/:id", validateSchema(findById, "params"), controller.findById.bind(controller));
+router.post(
+  "/users/",
+  validateSchema(createUserSchema, "body"),
+  controller.create.bind(controller),
+);
 
 export default router;
