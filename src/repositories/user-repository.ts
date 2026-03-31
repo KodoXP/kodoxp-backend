d2ab8a39c8d5f552355c50563ab3b36e8299ac13
Peixotim <pedropeixotovz@gmail.com>
2026-03-31 11:45:43 -0300

    feat: Implements methods for searching for users in the repository.

--- src/repositories/user-repository.ts
+++ src/repositories/user-repository.ts
@@ -1,1 +1,13 @@
-export class UserRepository {}
+import User from "@/models/user";
+
+export class UserRepository {
+  public async findAll(): Promise<User[]> {
+    return await User.findAll();
+  }
+
+  public async findById(id: string): Promise<User | null> {
+    return await User.findOne({
+      where: { id },
+    });
+  }
+}
