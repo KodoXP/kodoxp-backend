af2fb4b48d030e3f8a2f78ecb81b497c2a8907a6
Peixotim <pedropeixotovz@gmail.com>
2026-03-31 16:25:07 -0300

    feat: add create method and existsByEmail check to UserRepository

--- src/repositories/user-repository.ts
+++ src/repositories/user-repository.ts
@@ -1,6 +1,11 @@
+import { UsersCreate } from "@/dtos/user-dto";
 import User from "@/models/user";
 
 export class UserRepository {
+  public async create(create: UsersCreate): Promise<User> {
+    return await User.create(create);
+  }
+
   public async findAll(): Promise<User[]> {
     return await User.findAll();
   }
@@ -9,5 +14,13 @@ export class UserRepository {
     return await User.findOne({
       where: { id },
     });
+  }
+
+  public async existsByEmail(email: string): Promise<boolean> {
+    const count = await User.count({
+      where: { email },
+    });
+
+    return count > 0;
   }
 }
