eee135ef347c463a2595d439736b0579acca34d8
Peixotim <pedropeixotovz@gmail.com>
2026-03-31 11:46:25 -0300

    feat: Adds user routes with schema validation.

---
+++ src/routes/user-routes.ts
@@ -0,0 +1,15 @@
+import { UserController } from "@/controllers/user-controller";
+import { validateSchema } from "@/middlewares/validate-schema";
+import { UserRepository } from "@/repositories/user-repository";
+import { findById } from "@/schemas/user-schemas";
+import { UserService } from "@/services/user-service";
+import { Router } from "express";
+
+const router = Router();
+const userRepostiory = new UserRepository();
+const userService = new UserService(userRepostiory);
+const controller = new UserController(userService);
+
+router.get("/users/:id", validateSchema(findById), controller.findById.bind(controller));
+
+export default router;
