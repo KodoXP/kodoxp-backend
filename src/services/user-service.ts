f767207c90b1db013610ed7bc46ec1e67c2ce98a
Peixotim <pedropeixotovz@gmail.com>
2026-03-31 11:47:14 -0300

    feat: Adds verification and creation of the migration directory.

--- src/services/user-service.ts
+++ src/services/user-service.ts
@@ -1,1 +1,17 @@
-export class UserService {}
+import { UsersAttributes } from "@/dtos/user-dto";
+import { NotFoundError } from "@/exceptions";
+import { UserRepository } from "@/repositories/user-repository";
+
+export class UserService {
+  constructor(private readonly userRepository: UserRepository) {}
+
+  public async findById(id: string): Promise<UsersAttributes> {
+    const user = await this.userRepository.findById(id);
+
+    if (!user) {
+      throw new NotFoundError(`User with ID ${id} was not found.`);
+    }
+
+    return user;
+  }
+}
