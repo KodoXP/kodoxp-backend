43948d36ae8faa38c705373a4c4c3f4e01d393fe
italo <italo.devv@gmail.com>
2026-04-01 18:03:32 -0300

    feat: implement RewardService with CRUD operations and error handling

--- src/services/reward-service.ts
+++ src/services/reward-service.ts
@@ -1,1 +1,32 @@
-export class RewardService {}
+import { RewardAttributes, RewardCreate } from "@/dtos/rewards-dto";
+import { NotFoundError } from "@/exceptions";
+import { RewardRepository } from "@/repositories/reward-repository";
+import Rewards from "@/models/rewards";
+
+export class RewardService {
+  constructor(private readonly rewardsRepository: RewardRepository) {}
+
+  public async create(request: RewardCreate): Promise<RewardAttributes> {
+    return await this.rewardsRepository.create(request);
+  }
+
+  public async findAll(): Promise<Rewards[]> {
+    return await this.rewardsRepository.findAll();
+  }
+
+  public async findById(id: string): Promise<RewardAttributes> {
+    const reward = await this.rewardsRepository.findById(id);
+    if (!reward) {
+      throw new NotFoundError(`Reward with ID ${id} was not found.`);
+    }
+    return reward;
+  }
+
+  public async delete(id: string): Promise<boolean> {
+    const foundedReward = await this.rewardsRepository.findById(id);
+    if (!foundedReward) {
+      throw new NotFoundError(`Reward with ID ${id} was not found.`);
+    }
+    return await this.rewardsRepository.delete(id);
+  }
+}
