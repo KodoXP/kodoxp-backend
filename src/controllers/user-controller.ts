d49f545ac240609805dcb1f39ac396a02ad93579
Peixotim <pedropeixotovz@gmail.com>
2026-04-01 14:37:28 -0300

    refactor: reorganize methods in the UserController to improve readability.

--- src/controllers/user-controller.ts
+++ src/controllers/user-controller.ts
@@ -8,7 +8,25 @@ export class UserController {
     try {
       const { id } = req.params;
       const user = await this.userService.findById(id);
+      res.status(200).json(user);
+    } catch (error) {
+      next(error);
+    }
+  }
 
+  public async findAll(req: Request, res: Response, next: NextFunction) {
+    try {
+      const users = await this.userService.findAll();
+      res.status(200).json(users);
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  public async findByEmail(req: Request, res: Response, next: NextFunction) {
+    try {
+      const { email } = req.params;
+      const user = await this.userService.findByEmail(email);
       res.status(200).json(user);
     } catch (error) {
       next(error);
@@ -17,8 +35,18 @@ export class UserController {
 
   public async create(req: Request, res: Response, next: NextFunction) {
     try {
-      const create = await this.userService.create(req.body);
-      res.status(201).json(create);
+      const user = await this.userService.create(req.body);
+      res.status(201).json(user);
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  public async deactivate(req: Request, res: Response, next: NextFunction) {
+    try {
+      const { id } = req.params;
+      await this.userService.deactivate(id);
+      res.status(204).send();
     } catch (error) {
       next(error);
     }
