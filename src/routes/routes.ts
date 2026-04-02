c70873a56f3fcf82b7df77c668220b9fb98b35fc
italo <italo.devv@gmail.com>
2026-04-02 10:20:48 -0300

    feat: add reward routes to main router

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,9 +1,11 @@
 import { Router } from "express";
 import healthRouters from "@/routes/health";
 import userRouters from "@/routes/user-routes";
+import rewardRoutes from "@/routes/reward-routes";
 const router = Router();
 
 router.use(healthRouters);
 router.use(userRouters);
+router.use(rewardRoutes);
 
 export default router;
