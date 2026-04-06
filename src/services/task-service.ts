8510761f3a6013a1566c6dedc07e704859e238a9
italo <italo.devv@gmail.com>
2026-04-06 10:29:16 -0300

    feat: implement TaskService with CRUD methods for task management

--- src/services/task-service.ts
+++ src/services/task-service.ts
@@ -1,1 +1,31 @@
-export class TaskService {}
+import { NotFoundError } from "@/exceptions";
+import { TasksAttributes, TasksCreate } from "@/dtos/tasks-dto";
+import { TaskRepository } from "@/repositories/task-repository";
+
+export class TaskService {
+  constructor(private readonly tasksRespository: TaskRepository) {}
+
+  public async findById(id: string): Promise<TasksAttributes> {
+    const tasks = await this.tasksRespository.findById(id);
+    if (!tasks) {
+      throw new NotFoundError(`Task with ID ${id} was not found.`);
+    }
+    return tasks;
+  }
+
+  public async findAll(): Promise<TasksAttributes[]> {
+    return await this.tasksRespository.findAll();
+  }
+
+  public async create(request: TasksCreate): Promise<TasksAttributes> {
+    return await this.tasksRespository.create(request);
+  }
+
+  public async delete(id: string): Promise<boolean> {
+    const foundedTask = await this.tasksRespository.findById(id);
+    if (!foundedTask) {
+      throw new NotFoundError(`Task with ID ${id} was not found.`);
+    }
+    return await this.tasksRespository.delete(id);
+  }
+}
